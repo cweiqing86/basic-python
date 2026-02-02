5cf10af42e677e3ab40b85a243df2e69c96ee619
cweiqing86 <weiqingwq@hotmail.com>
2026-02-02 11:18:49 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,28 @@
 print("Hi i need to go to the toilet")
 # BMI ?
 # Height ?
+# BMI = weight / (height * height)
+#Str - > function to convert numbers tro string values
+# string is text we enclose using quote
+'''
+multi line comment
+'''
+height = 1.65 #float
+weight = 65 #integers -> whole number
+bmi = weight / (height * height)
+print("your bmi is" + str(bmi))
+# string concatenation
+quote ="Monday bluezz" #string variable
+quote ="Tues is bluezz"
+print("today is " + quote) #quote is a string
+#basic data types in python
+a = 10 #integers -> whole numbers
+b = 3.14 #floats -> decimal values
+c = True #booleans -> True or False | Yes or No |
+d = "this is a string" #string -> text values
+e = 'this is also a string'
+print("enter your height")
+height = float(input("Enter height in metres"))
+weight = float(input("Enter weight in kg"))
+bmi = weight / (height * height)
+print("your bmi is" + str(bmi))
